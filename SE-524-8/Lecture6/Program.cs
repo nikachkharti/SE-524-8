2030228f75d4e93d0be025e6972f1dad34ac6c80
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-01-24 21:22:42 +0400

    String methods

---
+++ SE-524-8/Lecture6/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Globalization;
+
+namespace Lecture6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+            string name = "Nikoloz"; // name.Length = 7
+            int age = 30;
+
+            //string a = "Hello";
+            //string b = "hello";
+
+            Console.WriteLine($"Hello {name}"); //Hello Nikoloz
+            Console.WriteLine("Hello" + " " + name); //Hello Nikoloz
+            Console.WriteLine("Hello {0} {1}", name, age); //Hello Nikoloz 30
+
+            //var result = a.Equals(b, StringComparison.OrdinalIgnoreCase);
+
+            //var result = a.ToLower() == b.ToLower();
+
+            Console.ReadLine();
+
+
+
+            //string[] fruits = ["Apple", "Banana", "Orange"];
+            // "Apple,Banana,Orange"
+
+            //string joinedFruits = string.Join(", ", fruits);
+
+
+
+
+            //string[] fruitsArray = fruits.Split(',', 'a');
+
+
+
+            //text2 = text2.Replace("Javascript", "C#");
+
+
+            //name = name.Substring(0,4); // Niko
+            //name = name.Substring(0, name.Length);
+
+            //int indexOfN = name.IndexOf('N');
+            //int lastIndexOfN = name.LastIndexOf('N');
+
+
+
+
+            //var result = name.StartsWith("Ni");
+            //var result2 = name.EndsWith("zi");
+
+            //var result = name.Contains("ni", StringComparison.OrdinalIgnoreCase);
+
+            //string upperName = name.ToUpper();
+            //string lowerName = name.ToLower();
+
+            //name = name.TrimStart();
+            //name = name.TrimEnd();
+            //name = name.Trim();
+
+
+
+
+            //string nameEmpty = string.Empty;
+            //string nameEmpty = "";
+            //string newName = new string('A', 5); !!!
+
+            //char firstLetter = name[0];
+            //char lastLetter = name[name.Length - 1];
+
+
+             */
+
+
+
+        }
+    }
+}
