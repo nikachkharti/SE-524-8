c0890cabe40b9858198d747791d9dca47e6901cf
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-03-05 21:45:35 +0400

    MiniBank #1

---
+++ SE-524-8/MiniBank.ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using MiniBank.Repository;
+
+namespace MiniBank.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerRepository customerRepository = new CustomerRepository();
+            var singleCustomer = customerRepository.GetCustomer(9);
+            var allCustomers = customerRepository.GetCustomers();
+
+            var addResult = customerRepository.AddCustomer(new Repository.Models.Customer()
+            {
+                Id = 30,
+                Name = "Nika Chkhartishvili",
+                Email = "nika@gmail.com",
+                IdentityNumber = "12345678945",
+                PhoneNumber = "558774499",
+                CustomerType = 0 //PHYISICAL PERSON
+            });
+
+        }
+    }
+}
---
+++ SE-524-8/MiniBank.Repository/CustomerRepository.cs
@@ -0,0 +1,84 @@
+﻿using MiniBank.Repository.Models;
+
+namespace MiniBank.Repository
+{
+    public class CustomerRepository
+    {
+        private const string _filePath = @"../../../../MiniBank.Data/Customers.csv";
+        private readonly List<Customer> _customers;
+
+        public CustomerRepository()
+        {
+            _customers = LoadData(_filePath);
+        }
+
+
+        public int AddCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id) => _customers.FirstOrDefault(c => c.Id == id);
+
+        public List<Customer> GetCustomers() => _customers;
+
+
+        public int UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #region HELPERS
+
+        private static List<Customer> LoadData(string filePath)
+        {
+            var customers = new List<Customer>();
+
+            if (!File.Exists(filePath))
+                return customers;
+
+            var lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var customer = FromCsv(line);
+                if (customer != null)
+                    customers.Add(customer);
+            }
+
+            return customers;
+        }
+        private static Customer FromCsv(string line)
+        {
+            var parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 6)
+                throw new FormatException("Customer format is invalid");
+
+            return new Customer()
+            {
+                Id = int.Parse(parts[0]),
+                Name = parts[1],
+                IdentityNumber = parts[2],
+                PhoneNumber = parts[3],
+                Email = parts[4],
+                CustomerType = int.Parse(parts[5])
+            };
+
+        }
+
+
+
+
+        #endregion
+    }
+}
