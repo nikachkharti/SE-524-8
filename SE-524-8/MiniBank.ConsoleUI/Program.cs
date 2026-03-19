930c95f2ac83dc14a5881591cd1fd05b0ec9641e
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-03-19 22:02:47 +0400

    Service layer added

--- SE-524-8/MiniBank.ConsoleUI/Program.cs
+++ SE-524-8/MiniBank.ConsoleUI/Program.cs
@@ -1,24 +1,9 @@
-﻿using MiniBank.Repository.Models;
-using MiniBank.Repository.Models.Enums;
-using MiniBank.Repository.Validators;
-
-namespace MiniBank.ConsoleUI
+﻿namespace MiniBank.ConsoleUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Customer newCustomer = new();
-            newCustomer.Id = -1;
-            newCustomer.Name = string.Empty;
-            newCustomer.Email = "invalid-email";
-            newCustomer.IdentityNumber = "123";
-            newCustomer.PhoneNumber = "123";
-            newCustomer.CustomerType = CustomerType.Phyisical;
-
-
-
-            Validator.Validate(newCustomer);
         }
     }
 }
