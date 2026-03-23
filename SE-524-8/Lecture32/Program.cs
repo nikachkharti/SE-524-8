c006e3518e9dd59fb1e39de50c350ac16223cc7b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-03-23 21:57:57 +0400

    Multithreading

--- SE-524-8/Lecture32/Program.cs
+++ SE-524-8/Lecture32/Program.cs
@@ -1,13 +1,81 @@
-﻿namespace Lecture32
+﻿using System.Net.Sockets;
+
+namespace Lecture32
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
 
-
         }
 
+        #region Race Condition
+
+        //private static int numberToIncrease = 0;
+        //private static object locker = new object();
+
+        //Thread t1 = new(Increment);
+        //Thread t2 = new(Increment);
+        //Thread t3 = new(Increment);
+
+        //t1.Start();
+        //t2.Start();
+        //t3.Start();
+
+        //t1.Join();
+        //t2.Join();
+        //t3.Join();
+
+        //Console.WriteLine($"Actual: {numberToIncrease}"); //300000
+
+        //private static void Increment()
+        //{
+        //    lock (locker)
+        //    {
+        //        for (int i = 0; i < 100000; i++)
+        //        {
+        //            numberToIncrease++;
+        //        }
+        //    }
+        //}
+
+        #endregion
+
+
+        #region Split Array In Threads Example
+
+        /*
+            int[] ar = [1, 2, 3, 4, 5, 6, 7, 8];
+
+            ProcessArray(ar);
+            Console.WriteLine(string.Join(", ", ar));
+         */
+
+        private static void ProcessArray(int[] ar)
+        {
+            int mid = ar.Length / 2;
+
+            Thread t1 = new(() => Pow(ar, 0, mid));
+            Thread t2 = new(() => Pow(ar, mid, ar.Length));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+        }
+        private static void Pow(int[] ar, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                Thread.Sleep(1000);
+                ar[i] *= 2;
+            }
+        }
+
+        #endregion
 
 
         #region Multithreading Example
