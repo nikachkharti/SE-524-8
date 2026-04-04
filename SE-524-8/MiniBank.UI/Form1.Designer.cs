11a7085efaf6ec030681593f540c97e1b9dae924
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-04-04 21:17:07 +0400

    Form button fix

--- SE-524-8/MiniBank.UI/Form1.Designer.cs
+++ SE-524-8/MiniBank.UI/Form1.Designer.cs
@@ -158,6 +158,7 @@
             deleteCustomerBtn.TabIndex = 4;
             deleteCustomerBtn.Text = "Delete Customer";
             deleteCustomerBtn.UseVisualStyleBackColor = false;
+            deleteCustomerBtn.Click += deleteCustomerBtn_Click_1;
             // 
             // updateCustomerBtn
             // 
@@ -168,6 +169,7 @@
             updateCustomerBtn.TabIndex = 4;
             updateCustomerBtn.Text = "Update Customer";
             updateCustomerBtn.UseVisualStyleBackColor = false;
+            updateCustomerBtn.Click += updateCustomerBtn_Click_1;
             // 
             // clearFormBtn
             // 
@@ -179,6 +181,7 @@
             clearFormBtn.TabIndex = 4;
             clearFormBtn.Text = "Clear";
             clearFormBtn.UseVisualStyleBackColor = false;
+            clearFormBtn.Click += clearFormBtn_Click_1;
             // 
             // Form1
             // 
--- SE-524-8/MiniBank.UI/Form1.cs
+++ SE-524-8/MiniBank.UI/Form1.cs
@@ -8,13 +8,11 @@ namespace MiniBank.UI
     public partial class Form1 : Form
     {
         private readonly ICustomerService _customerService;
-        private readonly IAccountService _accountService;
 
-        public Form1(ICustomerService customerService, IAccountService accountService)
+        public Form1(ICustomerService customerService)
         {
             InitializeComponent();
             _customerService = customerService;
-            _accountService = accountService;
         }
 
 
@@ -23,8 +21,6 @@ namespace MiniBank.UI
             var customers = await _customerService.GetAllCustomersAsync();
             listBox1.DataSource = customers;
             listBox1.DisplayMember = "Name";
-
-            _accountService.Test();
         }
 
         private async void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -39,45 +35,6 @@ namespace MiniBank.UI
                 customerTypeCombo.DataSource = Enum.GetValues(typeof(CustomerType));
                 customerTypeCombo.SelectedItem = selectedCustomer.CustomerType;
             }
-        }
-
-        private async void deleteCustomerBtn_Click(object sender, EventArgs e)
-        {
-            var selectedCustomer = listBox1.SelectedItem as Service.Dtos.Customer.GetCustomerDto;
-            if (selectedCustomer != null)
-            {
-                await _customerService.DeleteCustomerAsync(selectedCustomer.Id);
-                await LoadCustomersAsync();
-            }
-        }
-
-        private async void updateCustomerBtn_Click(object sender, EventArgs e)
-        {
-            var selectedCustomer = listBox1.SelectedItem as Service.Dtos.Customer.GetCustomerDto;
-            if (selectedCustomer != null)
-            {
-                var updateCustomerDto = new UpdateCustomerDto()
-                {
-                    Id = selectedCustomer.Id,
-                    Name = nameValue.Text,
-                    IdentityNumber = idValue.Text,
-                    PhoneNumber = phoneNumberValue.Text,
-                    Email = emailValue.Text,
-                    CustomerType = (CustomerType)customerTypeCombo.SelectedItem
-                };
-                await _customerService.UpdateCustomerAsync(updateCustomerDto);
-                await LoadCustomersAsync();
-            }
-        }
-
-        private void clearFormBtn_Click(object sender, EventArgs e)
-        {
-            nameValue.Text = string.Empty;
-            phoneNumberValue.Text = string.Empty;
-            idValue.Text = string.Empty;
-            emailValue.Text = string.Empty;
-            customerTypeCombo.SelectedIndex = 0;
-
         }
 
 
@@ -102,6 +59,44 @@ namespace MiniBank.UI
 
             await _customerService.AddCustomerAsync(createCustomerDto);
             await LoadCustomersAsync();
+        }
+
+        private async void deleteCustomerBtn_Click_1(object sender, EventArgs e)
+        {
+            var selectedCustomer = listBox1.SelectedItem as Service.Dtos.Customer.GetCustomerDto;
+            if (selectedCustomer != null)
+            {
+                await _customerService.DeleteCustomerAsync(selectedCustomer.Id);
+                await LoadCustomersAsync();
+            }
+        }
+
+        private async void updateCustomerBtn_Click_1(object sender, EventArgs e)
+        {
+            var selectedCustomer = listBox1.SelectedItem as Service.Dtos.Customer.GetCustomerDto;
+            if (selectedCustomer != null)
+            {
+                var updateCustomerDto = new UpdateCustomerDto()
+                {
+                    Id = selectedCustomer.Id,
+                    Name = nameValue.Text,
+                    IdentityNumber = idValue.Text,
+                    PhoneNumber = phoneNumberValue.Text,
+                    Email = emailValue.Text,
+                    CustomerType = (CustomerType)customerTypeCombo.SelectedItem
+                };
+                await _customerService.UpdateCustomerAsync(updateCustomerDto);
+                await LoadCustomersAsync();
+            }
+        }
+
+        private void clearFormBtn_Click_1(object sender, EventArgs e)
+        {
+            nameValue.Text = string.Empty;
+            phoneNumberValue.Text = string.Empty;
+            idValue.Text = string.Empty;
+            emailValue.Text = string.Empty;
+            customerTypeCombo.SelectedIndex = 0;
         }
     }
 }
